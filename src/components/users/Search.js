2c59aff7451ded15a9299b23cfde42fab1fcb8e1
Wesley Osborne <wao218@lehigh.edu>
2020-04-09 11:19:04 -0400

    Added search function to Search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
     text: ''
+  }
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
   }
 
   onChange = (e) => {
@@ -11,7 +16,8 @@ class Search extends Component {
   
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.text); 
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   }
 
   render() {
