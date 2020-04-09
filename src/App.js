b69d78e40467514ce557185b0498105a1f3e49f3
Wesley Osborne <wao218@lehigh.edu>
2020-04-09 11:55:18 -0400

    Added Alert Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // Search Github Users
@@ -25,17 +27,26 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   }
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type }});
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
+
   render() {
     const { users, loading } = this.state;
-    
+
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search 
             searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users}/>
         </div>
