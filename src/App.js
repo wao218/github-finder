26e4e43013ee8e60c27eb9e3e4765ae803dfcbf5
Wesley Osborne <wao218@lehigh.edu>
2020-04-12 17:27:18 -0400

    moved user state to context

--- src/App.js
+++ src/App.js
@@ -13,21 +13,9 @@ import GithubState from './context/github/GithubState';
 import './App.css';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null); 
-
-  // Get Single Github User
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    setUser(res.data);
-    setLoading(false);
-  }
 
   // Get users repos
   const getUserRepos = async (username) => {
@@ -36,12 +24,6 @@ const App = () => {
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
     setRepos(res.data);
-    setLoading(false);
-  }
-
-  // Clear users from state
-  const clearUsers = () => {
-    setUser([]);
     setLoading(false);
   }
 
@@ -62,21 +44,16 @@ const App = () => {
               <Route exact path='/' render={(props) => (
                 <Fragment>
                   <Search
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
                     setAlert={showAlert}
                   />
-                  <Users loading={loading} users={users}/>
+                  <Users />
                 </Fragment>
               )}/>
               <Route exact path='/about' component={About} />
               <Route exact path='/user/:login' render={(props) => (
                 <User 
-                  { ...props } 
-                  getUser={getUser} 
-                  getUserRepos={getUserRepos} 
-                  user={user} 
-                  loading={loading} 
+                  { ...props }  
+                  getUserRepos={getUserRepos}
                   repos={repos}
                 />
               )} />
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ loading, repos, user, getUser, getUserRepos, match }) => {
+const User = ({ repos, getUserRepos, match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, loading, user } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -89,10 +94,7 @@ const User = ({ loading, repos, user, getUser, getUserRepos, match }) => {
 }
 
 User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
   repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
   getUserRepos: PropTypes.func.isRequired
 }
 
