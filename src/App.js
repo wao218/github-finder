5d3810fc33843ee7eca8959475234b301f833bf3
Wesley Osborne <wao218@lehigh.edu>
2020-04-11 23:26:09 -0400

    Added Reducers and Actions for set_loading and search_users

--- src/App.js
+++ src/App.js
@@ -19,16 +19,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null); 
 
-  // Search Github Users
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    setUsers(res.data.items);
-    setLoading(false);
-  }
-
   // Get Single Github User
   const getUser = async (username) => {
     setLoading(true);
@@ -71,8 +61,7 @@ const App = () => {
             <Switch>
               <Route exact path='/' render={(props) => (
                 <Fragment>
-                  <Search 
-                    searchUsers={searchUsers} 
+                  <Search
                     clearUsers={clearUsers}
                     showClear={users.length > 0 ? true : false}
                     setAlert={showAlert}
