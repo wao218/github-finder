5542013b471a7a4453c03bd6d2b6f9c092a29cd0
Wesley Osborne <wao218@lehigh.edu>
2020-04-12 20:17:04 -0400

    Added Home and NotFound components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import Search from '../users/Search';
+import Users from '../users/Users';
 
 const Home = () => {
   return (
-    <Fragment >
-
+    <Fragment>
+      <Search />
+      <Users />
     </Fragment>
   );
 }
